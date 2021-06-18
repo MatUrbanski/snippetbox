35978a5cc63fa4aa9b037b0537d71a6f6791ef11
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2021-06-18 14:14:01 +0200

    Project Structure and Organization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "fmt"
-  "log"
   "net/http"
   "strconv"
 )
@@ -61,27 +60,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
   }
 
   w.Write([]byte("Create a specific snippet..."))
-}
-
-func main() {
-  // Use the http.newServeMux() function to initialize a new servemux.
-  mux := http.NewServeMux()
-
-  // Register the home function as the handler for the "/" URL pattern.
-  mux.HandleFunc("/", home)
-
-  // Register the showSnippet function as the handler for the "/snippet" URL pattern.
-  mux.HandleFunc("/snippet", showSnippet)
-
-  // Register the createSnippet function as the handler for the "/snippet/create" URL pattern.
-  mux.HandleFunc("/snippet/create", createSnippet)
-
-  // Use the http.ListenAndServe() function to start a new web server. We pass in
-  // two parameters: the TCP network addres to listen on (in this case ":4000")
-  // and the servemux we just created. If http.ListenAndServe() returns an error
-  // we use log.Fatal() function to log the error message and exit. Note
-  // that any error returned by http.ListenAndServe() is always non-nil.
-  log.Println("Starting server on :4000")
-  err := http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
 }
