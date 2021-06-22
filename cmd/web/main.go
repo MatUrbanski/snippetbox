bb6962d31078fa74ec4eef14e1f2b8588c0edd19
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2021-06-22 23:10:32 +0200

    Connectin Timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -115,10 +115,13 @@ func main() {
   // the ErrorLog field so that the server now uses the custom errorLog logger in
   // the event of any problems.
   srv := &http.Server{
-    Addr:      *addr,
-    ErrorLog:  errorLog,
-    Handler:   app.routes(),
-    TLSConfig: tlsConfig,
+    Addr:         *addr,
+    ErrorLog:     errorLog,
+    Handler:      app.routes(),
+    TLSConfig:    tlsConfig,
+    IdleTimeout:  time.Minute,
+    ReadTimeout:  5 * time.Second,
+    WriteTimeout: 10 * time.Second,
   }
 
   // Use the http.ListenAndServe() function to start a new web server. We pass in
