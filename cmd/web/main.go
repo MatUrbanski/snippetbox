ec734a644121bd5abdb24c96013962ad18f31f18
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2021-06-23 13:44:44 +0200

    Mocking Dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,7 +10,9 @@ import (
   "os"
   "time"
 
+  "mateuszurbanski/snippetbox/pkg/models"
   "mateuszurbanski/snippetbox/pkg/models/mysql"
+
   _ "github.com/go-sql-driver/mysql"
   "github.com/golangcollege/sessions"
 )
@@ -28,12 +30,20 @@ const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 
 // Define an application struct to hold the application-wide dependencies.
 type application struct {
-  errorLog     *log.Logger
-  infoLog      *log.Logger
-  session      *sessions.Session
-  snippets     *mysql.SnippetModel
+  errorLog *log.Logger
+  infoLog  *log.Logger
+  session  *sessions.Session
+  snippets interface {
+    Insert(string, string, string) (int, error)
+    Get(int) (*models.Snippet, error)
+    Latest() ([]*models.Snippet, error)
+  }
   templateCache map[string]*template.Template
-  users         *mysql.UserModel
+  users         interface {
+    Insert(string, string, string) error
+    Authenticate(string, string) (int, error)
+    Get(int) (*models.User, error)
+  }
 }
 
 func main() {
